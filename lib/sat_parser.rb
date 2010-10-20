7512f3383166d58b51ea5d601a5f40f9eba22ec4
buffpojken <daniel@sykewarrior.com>
2010-10-20 20:50:30 +0200

    fixed embarrassing bug

--- lib/sat_parser.rb
+++ lib/sat_parser.rb
@@ -4,14 +4,14 @@ class SatParser < EventMachine::Connection
     
   def post_init
     puts self.inspect
-    db = EventMachine::MySQL.new(:host => "localhost", :username => "root", :database => "keyhole_development")
+    @db = EventMachine::MySQL.new(:host => "localhost", :username => "root", :database => "keyhole_development")
     $channel << "{event:'connect', 'type':'tracker'}"
   end
   
   def receive_data(data)
     puts "Data received..."
     puts data.inspect
-    db.query("insert into locations(latitude, longitude) value('ninja', 'neger');")
+    @db.query("insert into locations(latitude, longitude) value('ninja', 'neger');")
     $channel << data
   end
   
