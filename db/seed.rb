346bab3ed6afa5f907cc1af376013dce7ea9a325
Daniel Sundström <daniel@sykewarrior.com>
2011-09-19 22:13:59 +0200

    fixed static path for websocket

--- db/seed.rb
+++ db/seed.rb
@@ -3,13 +3,3 @@ user = User.create({
   :password => "ninjamagick", 
   :admin => true  
 })
-
-session = user.sessions.create({
-  :name => "Testsession 1"
-})
-
-device = session.devices.create({
-  :name => "Thor", 
-  :imei => "342432423432", 
-  :color => "#d3f62f" 
-})
