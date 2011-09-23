ae12c86b4936f30387e8fe174029289f778e4f55
Daniel Sundström <daniel@sykewarrior.com>
2011-09-23 21:39:09 +0200

    twekas

--- lib/em-websocket/em-websocket.rb
+++ lib/em-websocket/em-websocket.rb
@@ -1,7 +1,6 @@
 $:.unshift(File.dirname(__FILE__) + '/../lib')
-require "eventmachine"
 
-puts 
+require "eventmachine"
 
 %w[ debugger websocket connection handler_factory handler handler75 handler76 ].each do |file|
   require "#{File.join(File.dirname(__FILE__), file)}"
